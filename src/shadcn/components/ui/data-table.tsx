5bea44ff77d48ee2ce3f3781f357e7ddf1188e68
munergenis <munergenis@gmail.com>
2025-06-29 14:18:12 +0200

    feat: add className prop to DataTable for customizable styling

--- src/shadcn/components/ui/data-table.tsx
+++ src/shadcn/components/ui/data-table.tsx
@@ -17,11 +17,13 @@ import {
 interface DataTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[];
   data: TData[];
+  className?: string;
 }
 
 export function DataTable<TData, TValue>({
   columns,
   data,
+  className,
 }: DataTableProps<TData, TValue>) {
   const table = useReactTable({
     data,
@@ -30,7 +32,7 @@ export function DataTable<TData, TValue>({
   });
 
   return (
-    <div className="rounded-md border">
+    <div className={`rounded-md border my-4 ${className}`}>
       <Table>
         <TableHeader>
           {table.getHeaderGroups().map((headerGroup) => (
