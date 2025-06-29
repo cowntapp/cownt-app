d2faac1699bdf4019c7d71aaf409cf4029c2b8f9
munergenis <munergenis@gmail.com>
2025-06-29 14:19:09 +0200

    feat: implement QueryBoundary component for handling loading and error states in queries

---
+++ src/shared/components/QueryBoundary.tsx
@@ -0,0 +1,45 @@
+import { useEffect, type ReactNode } from 'react';
+import type { UseQueryResult } from '@tanstack/react-query';
+import { Loader } from '@/shadcn/components/Loader/Loader';
+import { toast } from 'sonner';
+import { ErrorMessage } from './ErrorMessage';
+import { i18n_errors } from '../translations/translations';
+
+interface QueryBoundaryProps<T> {
+  query: UseQueryResult<T, Error>;
+  loaderComponent?: ReactNode;
+  errorType?: 'component' | 'toast';
+  errorComponent?: ReactNode;
+  errorText?: string;
+  children: (data: T) => ReactNode;
+}
+
+export const QueryBoundary = <T,>({
+  query,
+  loaderComponent,
+  errorType = 'component',
+  errorComponent,
+  errorText = i18n_errors[500],
+  children,
+}: QueryBoundaryProps<T>) => {
+  useEffect(() => {
+    if (query.isError && errorType === 'toast') {
+      toast.error(errorText);
+    }
+  }, [query.isError, errorText, errorType]);
+
+  if (query.isLoading) return loaderComponent ?? <Loader className="my-8" />;
+  if (query.isError && errorType === 'component')
+    return (
+      errorComponent ?? (
+        <ErrorMessage
+          title={errorText}
+          className="my-8"
+        />
+      )
+    );
+
+  if (!query.data) return null;
+
+  return children(query.data);
+};
