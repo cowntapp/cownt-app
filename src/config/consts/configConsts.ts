2e305dee60c9980983184e191440ee4d82ba1ce9
munergenis <munergenis@gmail.com>
2025-07-04 13:07:27 +0200

    fix: set HAS_LANDING to false in config constants

--- src/config/consts/configConsts.ts
+++ src/config/consts/configConsts.ts
@@ -1,7 +1,7 @@
 import { menuData } from '../data/appConfigData';
 
 export const APP_NAME = 'Cownt';
-export const HAS_LANDING = true;
+export const HAS_LANDING = false;
 export const HAS_WORKSPACES = !!menuData.workspaces?.length;
 export const ALLOW_REGISTER = false;
 export const LOCALE = 'ca-ES';
