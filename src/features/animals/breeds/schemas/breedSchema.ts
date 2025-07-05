a943d9cbddd5ced6eb54e0560ffd9bfdb4d90ef7
munergenis <munergenis@gmail.com>
2025-07-05 13:34:55 +0200

    fix: correct typo in breed name maximum character message

--- src/features/animals/breeds/schemas/breedSchema.ts
+++ src/features/animals/breeds/schemas/breedSchema.ts
@@ -5,5 +5,5 @@ export const breedSchema = z.object({
   value: z
     .string()
     .min(2, 'Nom de Raça ha de tenir 2 caràcters mínim')
-    .max(20, 'Nom de Raça pot tenir 20 caràcters mínim'),
+    .max(20, 'Nom de Raça pot tenir 20 caràcters màxim'),
 });
