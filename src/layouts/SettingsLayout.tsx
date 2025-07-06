8c9c779991461442410c1b8c0eaf507d64976b93
munergenis <munergenis@gmail.com>
2025-07-06 08:57:48 +0200

    feat: replace back link with Button component for improved styling

--- src/layouts/SettingsLayout.tsx
+++ src/layouts/SettingsLayout.tsx
@@ -1,5 +1,7 @@
 import { HAS_WORKSPACES } from '@/config/consts/configConsts';
 import { menuData } from '@/config/data/appConfigData';
+import { Button } from '@/shadcn/components/ui/button';
+import { ArrowLeft } from 'lucide-react';
 import { Link, Outlet, useLocation } from 'react-router';
 
 export const SettingsLayout = () => {
@@ -14,7 +16,15 @@ export const SettingsLayout = () => {
   return (
     <div>
       <header className="flex h-16 px-6 shrink-0 items-center gap-2">
-        <Link to={backPath}>&larr; Torna</Link>
+        <Button
+          asChild
+          variant={'link'}
+        >
+          <Link to={backPath}>
+            <ArrowLeft />
+            Torna
+          </Link>
+        </Button>
       </header>
       <div className="px-6 py-4">
         <Outlet />
