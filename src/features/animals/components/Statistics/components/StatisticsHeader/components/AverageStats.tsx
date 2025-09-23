80bc71dc1bcf69640d7eaf2ab1643acd1553877a
munergenis <munergenis@gmail.com>
2025-09-23 21:02:46 +0200

    fix: statistics average of averages with posible null

--- src/features/animals/components/Statistics/components/StatisticsHeader/components/AverageStats.tsx
+++ src/features/animals/components/Statistics/components/StatisticsHeader/components/AverageStats.tsx
@@ -8,12 +8,16 @@ export const AverageStats = ({ averageOfAverages }: AverageStatsProps) => {
   return (
     <div className="text-3xl py-8">
       <h1 className="font-bold">Mitjana Parts Ramat</h1>
-      <p className="text-primary">
-        {averageOfAverages} d{' '}
-        <span className="text-xl text-gray-500">
-          {daysToMonths(averageOfAverages)}
-        </span>
-      </p>
+      {averageOfAverages ? (
+        <p className="text-primary">
+          {averageOfAverages} d{' '}
+          <span className="text-xl text-gray-500">
+            {daysToMonths(averageOfAverages)}
+          </span>
+        </p>
+      ) : (
+        <p className="text-primary">-</p>
+      )}
     </div>
   );
 };
